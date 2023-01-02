152f8d307f282ac09bfb34bb9fba16971cf8c383
ThinhVD8 <vuducthinhkmt@gmail.com>
2023-01-02 10:14:58 +0700

    add lint lib, lintfix index.js in back end

--- back-end/index.js
+++ back-end/index.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const momgoose = require("mongoose");
-const dotenv= require('dotenv')
-const authRouter= require('./routers/auth')
-const userRouter= require('./routers/user')
-const productRouter=require('./routers/product')
-const productOrder=require('./routers/order')
-const productCart=require('./routers/cart')
-const cors = require('cors')
+const momgoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRouter = require('./routers/auth');
+const userRouter = require('./routers/user');
+const productRouter = require('./routers/product');
+const productOrder = require('./routers/order');
+const productCart = require('./routers/cart');
+const cors = require('cors');
 
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
 
-momgoose.connect(
-  process.env.MONGO_URL
-).then(()=>console.log('BD Connection Completed')).catch((err)=>console.log(err));
-app.use(cors())
-app.use("/auth",authRouter)
-app.use("/user",userRouter)
-app.use("/product",productRouter)
-app.use("/order",productOrder)
-app.use("/cart",productCart)
+momgoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('BD Connection Completed'))
+  .catch((err) => console.log(err));
+app.use(cors());
+app.use('/auth', authRouter);
+app.use('/user', userRouter);
+app.use('/product', productRouter);
+app.use('/order', productOrder);
+app.use('/cart', productCart);
 
-
-app.listen(process.env.PORT||5000, () => {
-  console.log("back-end");
+app.listen(process.env.PORT || 5000, () => {
+  console.log('back-end');
 });
