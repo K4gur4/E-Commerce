5d4aae2eafd64dc302cf7e198cb4ad78863e8acb
thắng <69190832+ngonngay@users.noreply.github.com>
2023-01-13 10:44:58 +0700

    refactor code

--- back-end/routers/auth.js
+++ back-end/routers/auth.js
@@ -3,58 +3,39 @@ const User = require("../models/user");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-//tạo tài khoản
 router.post("/register", async (req, res) => {
-  const newUser = {
-    username: req.body.username,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SEC_KEY
-    ).toString(),
-  };
   try {
-    const data = await User.create(newUser);
-    res.status(201).json({ NewUser: data });
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(req.body.password, process.env.SEC_KEY).toString(),
+    });
+    res.status(201).json({ newUser });
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err });
+    console.error(err);
+    res.status(500).json({ error: "Failed to create user" });
   }
 });
-// đăng nhập
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({
-      username: req.body.username,
-    });
+    const user = await User.findOne({ username: req.body.username });
     if (!user) {
-      return res.status(401).json({ message: "User not found!!" });
+      return res.status(401).json({ message: "User not found" });
     }
 
-    const passwordData = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.SEC_KEY
-    ).toString(CryptoJS.enc.Utf8);
-    if (passwordData !== req.body.password) {
-      return res.status(401).json({ message: "Wrong password!!!" });
+    const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.SEC_KEY).toString(CryptoJS.enc.Utf8);
+    if (decryptedPassword !== req.body.password) {
+      return res.status(401).json({ message: "Incorrect password" });
     }
 
-    const accsessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_KEY,
-      { expiresIn: "1d" }
-    );
+    const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_KEY, { expiresIn: "1d" });
 
-    console.log("log in conpleted");
-    const { password, ...other } = user._doc;
-    return res.status(200).json({ dataLogin: other, accsessToken });
+    const { password, ...userData } = user._doc;
+    return res.status(200).json({ userData, accessToken });
   } catch (err) {
-    console.log(err.message);
-    return res.status(500).json({ error: err });
+    console.error(err);
+    return res.status(500).json({ error: "Failed to login" });
   }
 });
 
